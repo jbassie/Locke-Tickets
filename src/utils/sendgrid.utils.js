13fa7e282f353b252d72987e845a32c0b949f552
Bassey John <jsbazman@gmail.com>
2024-06-10 13:14:56 +0100

    updates

--- src/utils/sendgrid.utils.js
+++ src/utils/sendgrid.utils.js
@@ -8,7 +8,7 @@ exports.sendOTPEmail = (user, OTP) => {
     const msg = {
         to: user.email, // Change to your recipient
         from: Config.SENDGRID_SENDER, // Change to your verified sender
-        subject: 'EZ Tickets password reset',
+        subject: 'Lockes Tickets password reset',
         templateId: 'd-a78a15d6feb442618af35cc1dfea2076',
         dynamic_template_data: {full_name: user.full_name, OTP}
     };
